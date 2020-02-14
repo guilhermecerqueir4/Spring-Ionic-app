dac6b3906233f25f3a9a4ffcb7708a5542dee9cb
guilherme Rocha <guilherme.rocha@absoftware.com.br>
2020-02-14 15:22:06 -0300

    Pedido,Pagamento,EstadoPagamento + ajuste reg. duplicados

--- src/main/java/com/guilherme/projSpring/domain/Cliente.java
+++ src/main/java/com/guilherme/projSpring/domain/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -105,6 +108,15 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -130,7 +142,8 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
+
+
 	
 	
 }
