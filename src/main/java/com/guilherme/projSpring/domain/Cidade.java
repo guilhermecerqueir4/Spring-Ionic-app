f93debb49e77b03131822bb0be480759937635ec
glmcerqueir4 <guilhermecerqueir4@gmail.com>
2020-02-14 01:58:53 -0300

    endpoint /clientes/{id} disponivel

--- src/main/java/com/guilherme/projSpring/domain/Cidade.java
+++ src/main/java/com/guilherme/projSpring/domain/Cidade.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Cidade implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -17,6 +19,8 @@ public class Cidade implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@JsonManagedReference //protegendo classe (Cidade) contra serialização cíclica com 'Estado'
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
