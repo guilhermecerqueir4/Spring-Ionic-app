626cffc45a8104ffce14d4930f3383f2ff0e0e25
glmcerqueir4 <guilhermecerqueir4@gmail.com>
2020-02-21 01:30:04 -0300

    ajuste Fase 1 - modelo conceitual

---
+++ src/main/java/com/guilherme/projSpring/domain/Pagamento.java
@@ -0,0 +1,93 @@
+package com.guilherme.projSpring.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.guilherme.projSpring.domain.enums.EstadoPagamento;
+
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED) //mapeando herança para as subclasses
+
+public abstract class Pagamento implements Serializable{  //classe abstrata para que nao seja possivel instanciar diretamente, e sim suas subclasses
+	private static final long serialVersionUID = 1L;
+
+	@Id //não gerar GenerateValue pois o ID da classe pagamento será o mesmo ID do Pedido. 
+	private Integer id;
+	private Integer estado;
+	
+	@JsonBackReference
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId // A chave PK do pagamento sera o ID do pedido.
+	private Pedido pedido;
+	
+	public Pagamento() {
+		
+	}
+
+	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
+		super();
+		this.id = id;
+		this.estado = estado.getCod();
+		this.pedido = pedido;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public EstadoPagamento getEstado() {
+		return EstadoPagamento.toEnum(estado);
+	}
+
+	public void setEstado(EstadoPagamento estado) {
+		this.estado = estado.getCod();
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pagamento other = (Pagamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
