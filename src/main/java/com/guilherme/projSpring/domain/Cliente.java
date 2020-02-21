d4b1d945681bc6f164943126066804d94a2f0789
glmcerqueir4 <guilhermecerqueir4@gmail.com>
2020-02-21 00:59:50 -0300

    teste

--- src/main/java/com/guilherme/projSpring/domain/Cliente.java
+++ src/main/java/com/guilherme/projSpring/domain/Cliente.java
@@ -37,9 +37,6 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
 	
-	@OneToMany(mappedBy="cliente")
-	private List<Pedido> pedidos = new ArrayList<>();
-	
 	public Cliente() {
 		
 	}
@@ -108,15 +105,6 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
-	
-	public List<Pedido> getPedidos() {
-		return pedidos;
-	}
-
-	public void setPedidos(List<Pedido> pedidos) {
-		this.pedidos = pedidos;
-	}
-	
 
 	@Override
 	public int hashCode() {
@@ -142,8 +130,7 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-
-
+	
 	
 	
 }
