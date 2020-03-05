fab4e87fcd1818a3abe84cdee924401be765b609
glmcerqueir4 <guilhermecerqueir4@gmail.com>
2020-03-04 23:20:44 -0300

    Validação de CNPJ e CPF na inserção de Cliente

--- src/main/java/com/guilherme/projSpring/dto/ClienteNewDTO.java
+++ src/main/java/com/guilherme/projSpring/dto/ClienteNewDTO.java
@@ -2,25 +2,44 @@ package com.guilherme.projSpring.dto;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.Length;
+
+import com.guilherme.projSpring.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	
-	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="Tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty
+	@Email(message="E-mail inválido")
 	private String email;
+	
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro	;
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
 	private String complemento;
 	private String bairro;	
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
--- src/main/java/com/guilherme/projSpring/services/ClienteService.java
+++ src/main/java/com/guilherme/projSpring/services/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 					repo.deleteById(id);
 					}
 			catch(DataIntegrityViolationException e){
-					throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
+					throw new DataIntegrityException("Não é possivel excluir porque há pedidos relacionadas");
 				}
 			}
 			
