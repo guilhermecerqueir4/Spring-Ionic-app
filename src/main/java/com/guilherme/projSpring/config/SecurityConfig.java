3444100888f6cd8fae848a779b3327735ab09f55
guilherme Rocha <guilherme.rocha@absoftware.com.br>
2020-03-10 18:18:39 -0300

    adicionando Spring Security na aplicação

---
+++ src/main/java/com/guilherme/projSpring/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.guilherme.projSpring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication
+.builders.AuthenticationManagerBuilder;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		builder.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER")
+		.and().
+		withUser("avanco").password("{noop}ads").roles("ADMIN");
+	}
+}
